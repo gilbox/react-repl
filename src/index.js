55609950733e5542d4ee1eccd4c28c8c1fe2936c
gilbox <birmangil@gmail.com>
2015-06-28 01:03:12 -0500

    split-view

--- src/index.js
+++ src/index.js
@@ -4,19 +4,15 @@ const Ace = require('./ace');
 const babel = require('babel-core/browser');
 const consoleLog = require('./hijack-log')();
 const PlayRun = require('./play-run');
-const SplitDragger = require('./split-dragger');
+const SplitView = require('./split-view');
 
 const debounce = (fn, ms, t) => () => {
   clearTimeout(t);
   t = setTimeout(fn, ms); };
 
 const styles = {
-  aceEditor: {
-    position: 'absolute',
-    top: '0',
-    left: '0',
-    height: '100%',
-    overflow: 'hidden',
+  fill: {
+    height: '100%'
   },
   replContainer: {
     position: 'relative',
@@ -31,40 +27,29 @@ const ReactREPL = React.createClass({
     splitDraggerSize: React.PropTypes.number,
     initialOrientation: React.PropTypes.oneOf(['vertical', 'horizontal'])
   },
+  
   getDefaultProps() {
     return {
       splitDraggerSize: 12,
       initialOrientation: 'vertical',
     }
   },
+
   getInitialState() {
     return {
       orientation: this.props.initialOrientation,
-      splitOffsetPercent: 'splitOffset' in this.props ? this.props.initialSplitOffsetPercent : 50,
       autoRun: true,
       code: this.props.initialCode,
     };
   },
+
   componentDidMount() {
     //const update = debounce(_=> this.forceUpdate(), 100);
     //this.refs.ace.editor.on('change', _=> this.state.autoRun && update());
   },
-  isVertical() {
-    return this.state.orientation === 'vertical'
-  },
-  handleSplitDrag(offset) {
-    const replContainerSize = React.findDOMNode(this.refs.repl)
-                                   .getBoundingClientRect()
-                                   [this.isVertical() ? 'width' : 'height'];
 
-    const splitOffsetPercent = this.state.splitOffsetPercent + 100 * offset / replContainerSize;
-    this.setState({ splitOffsetPercent });
-  },
   render() {
-    const isVertical = this.isVertical();
     const theCode = this.state.code;
-    const splitDraggerSize = this.props.splitDraggerSize;
-    const halfSplitDraggerSize = splitDraggerSize/2;
 
     consoleLog.clear();
 
@@ -78,56 +63,33 @@ const ReactREPL = React.createClass({
       console.log(e.stack);
     }
 
-    const changingPos = isVertical ? 'left' : 'top';
-    const fixedPos = isVertical ? 'top' : 'left';
-    const changingDim = isVertical ? 'width' : 'height';
-    const fixedDim = isVertical ? 'height' : 'width';
-
-    const aceEditorStyles = {
-      position: 'absolute',
-      overflow: 'hidden',
-      top: '0',
-      left: '0',
-      [fixedDim]: '100%',
-      [changingDim]: `calc(${this.state.splitOffsetPercent}% - ${halfSplitDraggerSize}px)` };
-
-    const splitDraggerProps = {
-      position: "absolute",
-      onDragEnd: this.handleSplitDrag,
-      orientation: this.state.orientation,
-      [changingPos]: `calc(${this.state.splitOffsetPercent}% - ${halfSplitDraggerSize}px)`,
-      [fixedDim]: '100%',
-      [changingDim]: splitDraggerSize };
-
-    const outputStyles = {
-      position: 'absolute',
-      overflow: 'hidden',
-      [fixedPos]: '0',
-      [fixedDim]: '100%',
-      [changingPos]: `calc(${this.state.splitOffsetPercent}% + ${splitDraggerSize}px)`,
-      [changingDim]: `calc(${this.state.splitOffsetPercent}% - ${halfSplitDraggerSize}px)` };
-
     return (
       <div style={styles.replContainer} ref="repl">
-        <div style={aceEditorStyles}>
-          <Ace
-            ref="ace"
-            name="aceEditor"
-            mode="javascript"
-            theme="monokai" />
-        </div>
 
-        <SplitDragger {...splitDraggerProps} />
+        <SplitView
+          style={styles.replContainer}
+          orientation={this.state.orientation}
+          splitDraggerSize={this.props.splitDraggerSize}>
 
-        <div style={outputStyles}>
-
-          <div>
-            <Console log={consoleLog.get() || null} />
+          <div style={styles.fill}>
+            <Ace
+              ref="ace"
+              name="aceEditor"
+              mode="javascript"
+              theme="monokai" />
           </div>
 
-          <PlayRun />
+          <div style={styles.fill}>
 
-        </div>
+            <div>
+              <Console log={consoleLog.get() || null} />
+            </div>
+
+            <PlayRun />
+          </div>
+
+        </SplitView>
+
       </div>
     );
   }
---
+++ src/split-view.js
@@ -0,0 +1,81 @@
+const React = require('react');
+const SplitDragger = require('./split-dragger');
+
+const SplitView = React.createClass({
+  propTypes: {
+    splitDraggerSize: React.PropTypes.number,
+    orientation: React.PropTypes.oneOf(['vertical', 'horizontal'])
+  },
+
+  getDefaultProps() {
+    return {
+      splitDraggerSize: 12,
+      orientation: 'vertical',
+      initialSplitOffsetPercent: 50,
+    }
+  },
+
+  getInitialState() {
+    return {
+      splitOffsetPercent: this.props.initialSplitOffsetPercent,
+    };
+  },
+
+  isVertical() {
+    return this.props.orientation === 'vertical'
+  },
+
+  handleSplitDrag(offset) {
+    const containerSize = React.findDOMNode(this.refs.container)
+                                   .getBoundingClientRect()
+                                   [this.isVertical() ? 'width' : 'height'];
+
+    const splitOffsetPercent = this.state.splitOffsetPercent + 100 * offset / containerSize;
+    this.setState({ splitOffsetPercent });
+  },
+
+  render() {
+    const isVertical = this.isVertical();
+    const splitDraggerSize = this.props.splitDraggerSize;
+    const halfSplitDraggerSize = splitDraggerSize/2;
+
+    const changingPos = isVertical ? 'left' : 'top';
+    const fixedPos = isVertical ? 'top' : 'left';
+    const changingDim = isVertical ? 'width' : 'height';
+    const fixedDim = isVertical ? 'height' : 'width';
+
+    const firstPaneStyles = {
+      position: 'absolute',
+      overflow: 'hidden',
+      top: '0',
+      left: '0',
+      [fixedDim]: '100%',
+      [changingDim]: `calc(${this.state.splitOffsetPercent}% - ${halfSplitDraggerSize}px)` };
+
+    const splitDraggerProps = {
+      position: "absolute",
+      onDragEnd: this.handleSplitDrag,
+      orientation: this.state.orientation,
+      [changingPos]: `calc(${this.state.splitOffsetPercent}% - ${halfSplitDraggerSize}px)`,
+      [fixedDim]: '100%',
+      [changingDim]: splitDraggerSize };
+
+    const secondPaneStyles = {
+      position: 'absolute',
+      overflow: 'hidden',
+      [fixedPos]: '0',
+      [fixedDim]: '100%',
+      [changingPos]: `calc(${this.state.splitOffsetPercent}% + ${splitDraggerSize}px)`,
+      [changingDim]: `calc(${this.state.splitOffsetPercent}% - ${halfSplitDraggerSize}px)` };
+
+    return (
+      <div style={this.props.style} ref="container">
+        <div style={firstPaneStyles}>{this.props.children[0]}</div>
+        <SplitDragger {...splitDraggerProps} />
+        <div style={secondPaneStyles}>{this.props.children[1]}</div>
+      </div>
+    );
+  }
+});
+
+module.exports = SplitView;
